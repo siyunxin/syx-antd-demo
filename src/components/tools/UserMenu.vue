8cdd4ceeea65aab5b6ecef3d90d3e4dd88d367cb
siyunxin <15194113617@163.com>
2019-07-19 14:13:26 +0800

    个人菜单路由添加

--- src/components/tools/UserMenu.vue
+++ src/components/tools/UserMenu.vue
@@ -1,27 +1,27 @@
 <template>
-    <div class="user-container">
-        <a-dropdown>
-            <a class="ant-dropdown-link" href="#">
-                <span>
-                    <a-avatar icon="user"/>
-                    <span class="user-name">司云昕</span>
-                </span>
-            </a>
-            <a-menu slot="overlay">
-                <a-menu-item>
-                    <router-link :to="{ name: 'mineindex' }">
-                        <a href="javascript:;">个人中心</a>
-                    </router-link>
-                </a-menu-item>
-                <a-menu-item>
-                    <a href="javascript:;">账户设置</a>
-                </a-menu-item>
-                <a-menu-item>
-                    <a href="javascript:;">退出登录</a>
-                </a-menu-item>
-            </a-menu>
-        </a-dropdown>
-    </div>
+  <div class="user-container">
+    <a-dropdown>
+      <a class="ant-dropdown-link" href="#">
+        <span>
+          <a-avatar icon="user" />
+          <span class="user-name">司云昕</span>
+        </span>
+      </a>
+      <a-menu slot="overlay">
+        <a-menu-item>
+          <router-link :to="{ name: 'mineindex' }">
+            <a href="javascript:;" @click="userCenter">个人中心</a>
+          </router-link>
+        </a-menu-item>
+        <a-menu-item>
+          <a href="javascript:;" @click="userSet">账户设置</a>
+        </a-menu-item>
+        <a-menu-item>
+          <a href="javascript:;" @click="logout">退出登录</a>
+        </a-menu-item>
+      </a-menu>
+    </a-dropdown>
+  </div>
 </template>
 
 <script>
@@ -29,7 +29,18 @@ export default {
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    userCenter() {
+      this.$router.replace("/mine/mineindex");
+    },
+    userSet() {
+      this.$router.replace("/mine/mineset");
+    },
+    logout() {
+      sessionStorage.clear();
+      this.$router.replace("/acount/login");
+    }
+  },
   mounted() {}
 };
 </script>
