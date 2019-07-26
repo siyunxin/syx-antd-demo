f2d5f7be4d9bb09cbf870a9e9841fa6a76076dbe
siyunxin <15194113617@163.com>
2019-07-26 11:01:35 +0800

    部分代码整理

--- src/components/layout/BasicLayout.vue
+++ src/components/layout/BasicLayout.vue
@@ -1,27 +1,34 @@
 <template>
-        <global-layout>
-            <bread-crumb style="padding:10px 10px 10px 0"></bread-crumb>
-            <router-view></router-view>
-        </global-layout>
+  <global-layout>
+    <bread-crumb style="padding:10px 10px 10px 0"></bread-crumb>
+    <router-view ></router-view>
+  </global-layout>
 </template>
 
 <script>
-import GlobalLayout from '@/components/layout/GlobalLayout';
-import BreadCrumb from '@/components/tools/BreadCrumb';
+import GlobalLayout from "@/components/layout/GlobalLayout";
+import BreadCrumb from "@/components/tools/BreadCrumb";
 export default {
-    data() {
-        return {
-            
-        }
-    },
-    components: {
-        GlobalLayout,
-        BreadCrumb
-    },
-    created() {
-
+  data() {
+    return {
+        transitionName: 'fade-enter'
+    };
+  },
+  components: {
+    GlobalLayout,
+    BreadCrumb
+  },
+  created() {},
+ /*  beforeRouteUpdate(to, from, next) {
+    const toDepth = to.path.split("/").length;
+    const fromDepth = from.path.split("/").length;
+    if(toDepth<fromDepth){
+        console.log('?')
     }
-}
+    this.transitionName = toDepth < fromDepth ? "fade-leave" : "fade-enter";
+    next();
+  } */
+};
 </script>
 
 <style>
